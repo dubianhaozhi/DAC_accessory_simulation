bbc96042ba20916b7510ccfda4dd6a37949c6e3c
渡辺豪志 <tuyosi.628@gmail.com>
2021-01-08 13:03:07 +0900

    drag, rotate, scale実装

---
+++ simulation/assets/js/move.js
@@ -0,0 +1,52 @@
+// 参考サイト: https://daybrush.com/moveable/release/latest/doc/
+
+const moveable = new Moveable(document.body, {
+  target: document.querySelector(".target"),
+  originDraggable: true,
+  originRelative: true,
+  draggable: true,
+  throttleDrag: 0,
+  startDragRotate: 0,
+  throttleDragRotate: 0,
+  zoom: 1,
+  origin: false,
+  padding: {"left":0,"top":0,"right":0,"bottom":0},
+  rotatable: true,
+  throttleRotate: 0,
+  rotationPosition: "top",
+  scalable: true,
+  keepRatio: true,
+  throttleScale: 0,
+  renderDirections: ["nw","ne","sw","se"],
+  edge: false,
+});
+let frame = {
+  translate: [0,0],
+  rotate: 0,
+  transformOrigin: "50% 50%",
+  scale: [1,1],
+};
+moveable.on("dragOriginStart", ({ dragStart }) => {
+  dragStart && dragStart.set(frame.translate);
+}).on("dragOrigin", ({ target, drag, transformOrigin }) => {
+  frame.translate = drag.beforeTranslate;
+  frame.transformOrigin = transformOrigin;
+}).on("dragStart", ({ set }) => {
+  set(frame.translate);
+}).on("drag", ({ target, beforeTranslate }) => {
+  frame.translate = beforeTranslate;
+}).on("rotateStart", ({ set }) => {
+  set(frame.rotate);
+}).on("rotate", ({ beforeRotate }) => {
+  frame.rotate = beforeRotate;
+}).on("scaleStart", ({ set }) => {
+  set(frame.scale);
+}).on("scale", ({ scale }) => {
+  frame.scale = scale
+}).on("render", ({ target }) => {
+  const { translate, rotate, transformOrigin, scale } = frame;
+  target.style.transformOrigin = transformOrigin;
+  target.style.transform = `translate(${translate[0]}px, ${translate[1]}px)`
+    + ` rotate(${rotate}deg)`
+    + ` scale(${scale[0]}, ${scale[1]}) `;
+});
