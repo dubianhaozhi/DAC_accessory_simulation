fda00183d311ef38830f230c64506f555fab80e9
渡辺豪志 <tuyosi.628@gmail.com>
2021-01-08 11:21:48 +0900

    スクショ実装

---
+++ simulation/assets/js/screen_shot.js
@@ -0,0 +1,15 @@
+// 参考サイト: https://qiita.com/youwht/items/8b681a856f59aa82d671
+
+$('#simulation__save').click(function() {
+  html2canvas($('#simulation__target'), {
+    onrendered: function(canvas) {
+      const imgData = canvas.toDataURL()
+      const a = document.createElement("a")
+      document.body.appendChild(a)
+      a.download = 'simulation.png'
+      a.href = imgData
+      a.click()
+      a.remove()
+    }
+  })
+})
